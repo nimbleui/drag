5b8b627b0a339aaf9c1112a7cef0a476e3179cfc
陈羽云 <897908015@qq.com>
2025-01-02 10:18:03 +0800

    fixed：修复依赖包

--- build/createPackage.ts
+++ build/createPackage.ts
@@ -4,7 +4,7 @@ import { projRoot } from './utils';
 
 const pkg = {
   name: '@nimble-ui/drag',
-  version: '1.2.4',
+  version: '1.2.5',
   description: 'low code drag',
   author: {
     name: 'Chen Yu Yun',
@@ -38,7 +38,7 @@ const pkg = {
   publishConfig: {
     access: 'public',
   },
-  peerDependencies: {
+  dependencies: {
     "@nimble-ui/move": "^1.0.4",
   },
   exports: {
